556e1a912983f5934b5867f1679c2595f7fac14d
Apostolos Tzinas <tzinas@tzinas.com>
2022-05-12 18:15:12 +0300

    Add database creation to configure_sb.sh

--- configure_db.sh
+++ configure_db.sh
@@ -1,3 +1,5 @@
+psql postgres -c "DROP DATABASE IF EXISTS db_class"
 psql postgres -c "DROP ROLE IF EXISTS db_class"
 
 psql postgres -c "CREATE USER db_class WITH ENCRYPTED PASSWORD 'db_class'"
+psql postgres -c "CREATE DATABASE db_class OWNER db_class"
