0c3d0829decfde63b5a3c866312d546c7e6cb3f4
Apostolos Tzinas <tzinas@tzinas.com>
2022-05-27 20:18:37 +0300

    Start delete api

--- app/pages/api/entities/[entity].ts
+++ app/pages/api/entities/[entity].ts
@@ -1,9 +1,10 @@
 import db from 'lib/db'
 import logger from 'lib/logger'
+import _ from 'lodash'
 
 type entity ={ [key: string]: string | number }
 
-type CreateEntityProps = {
+type EntityProps = {
   entity: string
   data: entity
   db: typeof db
@@ -14,7 +15,7 @@ type FetchAllPros = {
   db: typeof db
 }
 
-const createEntity = async ({ entity, data, db }: CreateEntityProps): Promise<void> => {
+const createEntity = async ({ entity, data, db }: EntityProps): Promise<void> => {
   await db`INSERT INTO ${db(entity)} ${db(data)}` // e.g. INSERT INTO Organization(name, ...) VALUES(Org1, ...)
 }
 
@@ -22,7 +23,27 @@ const fetchAll = ({ entity, db }: FetchAllPros): Promise<entity[]> => {
   return db`SELECT * FROM ${db(entity)}` // e.g. SELECT * FROM Organization
 }
 
+const deleteEntity = async ({ entity, data, db }: EntityProps): Promise<void> => {
+  const values = _.toPairs(data)
+  console.log(data)
+  //const response = await db`DELETE FROM ${db(entity)} WHERE organization_id = 200`
+  const response = await db`DELETE FROM ${db(entity)} ${db(data)}`
+    console.log(response)
+}
+
 const responses = {
+  DELETE: async (req, res) => {
+    const { entity } = req.query
+
+    try {
+      await deleteEntity({ entity, data: req.body, db })
+      logger.info(`${entity} deleted`, req.body)
+      return res.status(200).json({ message: `${entity} deleted` })
+    } catch (err) {
+      logger.debug(`Error deleting ${entity}:`, err)
+      return res.status(500).json({ err: `Error deleting ${entity}s` })
+    }
+  },
   GET: async (req, res) => {
     const { entity } = req.query
 
