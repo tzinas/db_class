dd7b5292812ce029df9da6ef1d8ebf6cf6458e0b
Apostolos Tzinas <tzinas@tzinas.com>
2022-05-28 01:24:00 +0300

    Fix docker errors

--- app/pages/entities/[entity].tsx
+++ app/pages/entities/[entity].tsx
@@ -53,9 +53,10 @@ const EntityPage: NextPage = () => {
     const res = await fetch(url)
 
     if (!res.ok) {
-      const error = new Error('An error occurred while fetching the data.')
-      error.info = await res.json()
-      error.status = res.status
+      const error = {
+        info: await res.json(),
+        status: res.status
+      }
       throw error
     }
     return res.json()
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,13 +2,10 @@ import type { NextPage } from 'next'
 import Table from 'components/Table'
 import Navigation from 'components/Navigation'
 
-import { headers, projects} from 'mock/projects'
-
 const HomePage: NextPage = () => {
   return (
     <>
       <Navigation />
-      <Table headers={headers} data={projects}></Table>
     </>
   )
 }
