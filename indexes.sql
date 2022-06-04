f05c3c212ff054dba3aa52f8ba38a1073589b2c6
rpanagiota <62808507+rpanagiota@users.noreply.github.com>
2022-06-04 23:19:18 +0300

    Update indexes

--- indexes.sql
+++ indexes.sql
@@ -1,5 +1,4 @@
 CREATE INDEX works_res_idx ON Works_on(researcher_id)
 CREATE INDEX works_proj_idx ON Works_on(project_id)
 CREATE INDEX proj_org_idx ON Project(organization_id)
-CREATE INDEX proj_org_idx ON Project(organization_id)
 CREATE INDEX conc_proj_idx ON Concerns(project_id)
